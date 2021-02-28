cb7859ca9143bed2bb18726bebb83845ce0ef9d0
kleddy17 <kleddy17@gmail.com>
2021-02-28 15:33:45 -0500

    react basics review

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  // gives us access to this.state
+  constructor() {
+    // super calls the constructor method on the component class
+    super();
+    this.state = {
+      string: "Hello Kristi Eddy"
+    };
+  }
+  render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {this.state.string}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <button 
+        onClick={()=> 
+        this.setState({string:"Hello Paul"})}>Change Text
+      </button>
       </header>
     </div>
-  );
+  ); }
 }
 
 export default App;
